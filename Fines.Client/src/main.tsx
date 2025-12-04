a8571e7f4ae88702d051270a1c156b161382e84b
Alfie Woodland <Alfie.Woodland@zenith.co.uk>
2025-12-04 15:16:53 +0000

    Add date picker component

--- Fines.Client/src/main.tsx
+++ Fines.Client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme.ts";
+import "@mantine/dates/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
